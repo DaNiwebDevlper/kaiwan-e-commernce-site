45d2038ee3a4204568769201fe95eae1202eaf08
Danish <dnazir343@gmail.com>
2024-08-01 10:56:51 +0500

    .

--- src/app/api/nextOrderId/route.js
+++ src/app/api/nextOrderId/route.js
@@ -1,26 +1,31 @@
-import { NextResponse } from "next/server"
-import { connectDB } from "../../../../libs/config/db"
+import { NextResponse } from "next/server";
+import { connectDB } from "../../../../libs/config/db";
 
 export async function GET() {
-    const { db } = await connectDB()
+    try {
+        const { db } = await connectDB();
 
-    // Fetch the last used order ID
-    const settings = await db.collection('orderId').findOne({ name: 'orderIdCounter' })
+        // Fetch the last used order ID
+        const settings = await db.collection('orderId').findOne({ name: 'orderIdCounter' });
 
-    let nextOrderId = 1
-    if (settings && settings.value) {
-        nextOrderId = settings.value + 1
+        let nextOrderId = 1;
+        if (settings && settings.value) {
+            nextOrderId = settings.value + 1;
+        }
+
+        // Update the order ID counter in the database
+        await db.collection('orderId').updateOne(
+            { name: 'orderIdCounter' },
+            { $set: { value: nextOrderId } },
+            { upsert: true }
+        );
+
+        // Format the order ID as an eight-digit string
+        const formattedOrderId = String(nextOrderId).padStart(8, '0');
+
+        return NextResponse.json({ orderId: formattedOrderId });
+    } catch (error) {
+        console.error("Error generating next order ID:", error);
+        return NextResponse.json({ error: "Error generating next order ID" }, { status: 500 });
     }
-
-    // Update the order ID counter in the database
-    await db.collection('orderId').updateOne(
-        { name: 'orderIdCounter' },
-        { $set: { value: nextOrderId } },
-        { upsert: true }
-    )
-
-    // Format the order ID as a four-digit string
-    const formattedOrderId = String(nextOrderId).padStart(8, '0')
-
-    return NextResponse.json({ orderId: formattedOrderId })
 }
