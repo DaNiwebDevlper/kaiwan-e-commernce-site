27bf9489124e00a3c2de6a327e9db44ba9e2fc6d
Dream_Devloper <111285835+DaNiwebDevlper@users.noreply.github.com>
2024-08-07 16:21:17 +0500

    Update delete route.js

--- src/app/api/delete_product/[id]/route.js
+++ src/app/api/delete_product/[id]/route.js
@@ -6,7 +6,8 @@ export async function DELETE(request, { params }) {
   try {
     const id = params.id;
 
-    const deleteProduct = await ProductModel.findByIdAndDelete(id);
+    // const deleteProduct = await ProductModel.findByIdAndDelete(id);
+    const  deleteProduct = await ProductModel.deleteOne({_id:id})
     console.log(deleteProduct);
     if (!deleteProduct) {
       return NextResponse.json({ msg: "DB Error" });
