f72f2a98003efcce44f60e36746477cbcecb9a94
Dream_Devloper <111285835+DaNiwebDevlper@users.noreply.github.com>
2024-08-07 15:28:04 +0500

    Update route.js

--- src/app/api/delete_product/[id]/route.js
+++ src/app/api/delete_product/[id]/route.js
@@ -1,19 +1,25 @@
+import "../../../../../libs/config/db";
 import { NextResponse } from "next/server";
-import { connectDB } from "../../../../../libs/config/db";
 import ProductModel from "../../../../../libs/models/ProductModel";
 
 export async function DELETE(request, { params }) {
-    try {
-        const id = params.id;
+  try {
+    const id = params.id;
 
-        await connectDB();
-        await ProductModel.findByIdAndDelete(id);
-        console.log(id)
-        return NextResponse.json({ msg: "Deleted Successfully" });
-    } catch (error) {
-        return NextResponse.json({
-            msg: "Something went wrong",
-            error
-        }, { status: 400 });
+    const deleteProduct = await ProductModel.findByIdAndDelete(id);
+    console.log(deleteProduct);
+    if (!deleteProduct) {
+      return NextResponse.json({ msg: "DB Error" });
     }
+
+    return NextResponse.json({ msg: "Deleted Successfully" });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        msg: "Something went wrong",
+        error,
+      },
+      { status: 400 }
+    );
+  }
 }
